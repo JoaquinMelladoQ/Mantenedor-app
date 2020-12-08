a73183fd2f49e5111d7781223b8a9aa4b86fcd59
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-08 12:50:18 -0300

    content was added to list : button edit and ul usuario1 usuario2

--- src/Components/Lista.jsx
+++ src/Components/Lista.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class Lista extends Component {
     render() {
         return(
-            'Lista'
+            <ul>
+                <li>Usuario 1 <button>Editar</button></li>
+                <li>Usuario 2 <button>Editar</button></li>
+            </ul>
         )
     }
 }
