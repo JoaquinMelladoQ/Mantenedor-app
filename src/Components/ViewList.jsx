2816d0ff2bf3f862544cb310ff346e5b2c4f8964
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-08 12:12:14 -0300

    eslint was added to git ignore

---
+++ src/Components/ViewList.jsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+export default ViewList extends Component {
+    render() {
+        return(
+             'vista de lista'
+        )
+    }
+}
