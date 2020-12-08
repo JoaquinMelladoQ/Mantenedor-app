9cd927b041bbb909f38b3a6f9e8aed34f233f7f2
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-08 18:23:33 -0300

    fixed bug

--- src/Components/UserForm.jsx
+++ src/Components/UserForm.jsx
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 
 export default class UserForm extends Component {
 
-    state = {}
+    state = {
+        errors: {}
+    }
+
     handleChange = ({ target }) => {
         this.setState({
             [target.name]: target.value,
@@ -11,16 +14,26 @@ export default class UserForm extends Component {
 
     handleSubmit = e => {
         e.preventDefault()
-        e.target.reset()
+        const { errors, ...sinErrors } = this.state
+        const result = validate(sinErrors)
+        this.setState({ errors: result })
+        if (!Object.keys(result).length) {
+            
+            e.target.reset()
+        }
+
     }
 
     render() {
-        
+        const { errors } = this.state
         return(
              <form onSubmit={this.handleSubmit} >
                  <input placeholder="nombre" name="name" onChange={this.handleChange} />
+                 {errors.name && <p>{errors.name}</p>}
                  <input placeholder="email" name="email" onChange={this.handleChange} />
+                 {errors.name && <p>{errors.name}</p>}
                  <input placeholder="website" name="website" onChange={this.handleChange} />
+                 {errors.name && <p>{errors.name}</p>}
                  <input type="submit" value="Enviar" />
              </form>
         )
