70ae7346569cb12f167689211d96c3f415e7ddd4
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-08 12:15:52 -0300

    fixed syntax error

--- src/Components/ViewList.jsx
+++ src/Components/ViewList.jsx
@@ -1,10 +1,9 @@
-import { render } from '@testing-library/react';
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-export default ViewList extends Component {
+export default class ViewList extends Component {
     render() {
         return(
-             'vista de lista'
+             'vista de form'
         )
     }
 }
