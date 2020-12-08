36cab53a25bbbc76d7b118803eb1c7575f830f21
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-08 12:08:04 -0300

    imports were made at app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserForm from './Components/UserForm';
+import ViewList from './Components/ViewList';
 
 class App extends Component {
+
+    state = {
+      ruta: 'lista',
+
+    }
+
     render() {
       return(
         <div className="App">
